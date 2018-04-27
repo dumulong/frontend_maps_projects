4abd9873d76764b0d9602e3f44f6bbd1437549b9
Denis Dumulong <dumulong@sbcglobal.net>
2018-04-27 09:56:43 -0500

    prep work

---
+++ scripts/map_project.js
@@ -0,0 +1,29 @@
+var map;
+
+function initMap() {
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 40.373242,
+            lng: -105.521494
+        },
+        zoom: 12
+    });
+
+    var tribeca = {
+        lat: 40.373242,
+        lng: -105.521494
+    };
+
+    var marker = new google.maps.Marker({
+        position: tribeca,
+        map: map,
+        title: 'First Maker'
+    });
+    var infowindow = new google.maps.InfoWindow({
+        content: 'blablabla...'
+    });
+    marker.addListener('click', function () {
+
+    })
+}
