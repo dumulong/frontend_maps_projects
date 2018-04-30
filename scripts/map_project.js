2113124579274ea8a99f9d107b2b0c41a3eb9575
Denis Dumulong <dumulong@sbcglobal.net>
2018-04-30 10:33:45 -0500

    good step forward

--- scripts/map_project.js
+++ scripts/map_project.js
@@ -1,20 +1,86 @@
-var map;
-var mapData = mapData || {};
-var markerList = [];
-var myWindow = null;
+var gMap = {}; //Objet used for the google maps API
+var mapData = mapData || {}; //Our initial data for the map
 
-var Marker = function (item) {
-    this.title = item.title;
-    this.position = item.position;
-    this.infoContent = item.infoContent;
-};
+(function () {
+
+    var self = this;
+
+    self.map = null;
+    var infoWindow = null;
+
+    var markerList = [];
+
+    //Starting here!
+    self.initMap = function () {
+
+        self.map = new google.maps.Map(document.getElementById('map'), mapData.mapOptions);
+
+        infoWindow = new google.maps.InfoWindow();
+
+        var bounds = new google.maps.LatLngBounds();
+        mapData.markers.forEach(function(marker) {
+            var gMark = self.addGMarker(marker, infoWindow);
+            bounds.extend(gMark.position);
+        });
+        self.map.fitBounds(bounds);
+
+        ko.applyBindings(new ViewModel());
+    };
+
+    self.addGMarker = function (marker) {
+
+        var mrk = new google.maps.Marker({
+            position: marker.position,
+            map: self.map,
+            title: marker.title,
+            animation: google.maps.Animation.DROP
+        });
+
+        markerList.push(mrk);
+
+        mrk.addListener('click', function () {
+            populateInfoWindow(this, infoWindow);
+        });
+
+        return mrk;
+    };
+
+    self.clearAllMarkers = function () {
+        markerList.forEach(function(marker) {
+            marker.setMap(null);
+        });
+    };
+
+    function populateInfoWindow(marker) {
+
+        if (infoWindow.marker !== marker) {
+            infoWindow.marker = marker;
+            infoWindow.setContent('<div>' + marker.title + '</div>');
+            infoWindow.open(self.map, marker);
+            infoWindow.addListener('closeclick', function () {
+                infoWindow.setMarker(null);
+            });
+        }
+
+    }
+
+    gMap = self;
+
+})();
+
 
 var ViewModel = function () {
 
     var self = this;
-    self.filter = ko.observable();
+    self.filter = ko.observable('');
 
     self.markerList = ko.observableArray([]);
+
+    var Marker = function (item) {
+        this.title = item.title;
+        this.position = item.position;
+        this.infoContent = item.infoContent;
+    };
 
     mapData.markers.forEach(function(marker) {
         self.markerList.push(new Marker(marker));
@@ -26,70 +92,25 @@ var ViewModel = function () {
 
         //Reset to a new list
         self.markerList = ko.observableArray([]);
+        gMap.clearAllMarkers ();
 
         //Now, if the title maches the filter, add it to our list
         var bounds = new google.maps.LatLngBounds();
-        var fltr = new RegExp(self.filter, 'i');
+        var fltr = new RegExp(self.filter(), 'i');
 
         mapData.markers.forEach(function(marker) {
-            if (self.filter !== '') {
-                if (fltr.test(marker.title)) {
-                    var gMark = self.markerList.push(new Marker(marker));
-                    bounds.extend(gMark.position);
-                }
+
+            var isMatch = true;
+            if (self.filter() !== '') { isMatch = fltr.test(marker.title); }
+
+            if (isMatch) {
+                self.markerList.push(new Marker(marker));
+                var gMarker = gMap.addGMarker(marker);
+                bounds.extend(gMarker.position);
             }
         });
 
-        map.fitBounds(bounds);
+        gMap.map.fitBounds(bounds);
     };
 
 };
-
-function addMarker(marker, infoWindow) {
-
-    var mrk = new google.maps.Marker({
-        position: marker.position,
-        map: map,
-        title: marker.title,
-        animation: google.maps.Animation.DROP
-    });
-
-    mrk.addListener('click', function () {
-        populateInfoWindow(this, infoWindow);
-    });
-
-    return mrk;
-}
-
-function populateInfoWindow(marker, infoWindow) {
-
-    if (infoWindow.marker !== marker) {
-        infoWindow.marker = marker;
-        infoWindow.setContent('<div>' + marker.title + '</div>');
-        infoWindow.open(map, marker);
-        infoWindow.addListener('closeclick', function () {
-            infoWindow.setMarker(null);
-        });
-    }
-
-}
-
-
-//Starting here!
-function initMap() {
-
-    map = new google.maps.Map(document.getElementById('map'), mapData.mapOptions);
-
-    myWindow = new google.maps.InfoWindow();
-
-    var bounds = new google.maps.LatLngBounds();
-    mapData.markers.forEach(function(marker) {
-        var gMark = addMarker(marker, myWindow);
-        bounds.extend(gMark.position);
-    });
-    map.fitBounds(bounds);
-
-    ko.applyBindings(new ViewModel());
-}
-
-
